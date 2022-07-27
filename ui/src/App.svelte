588f961bb938674b36f748d580e1e656707a9ac6
Jurij Retzlaff <jurij@topofweb.de>
2022-07-28 00:31:08 +0200

    daschboard page added

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -8,8 +8,14 @@
     Loading,
     SkipToContent,
   } from "carbon-components-svelte";
+  import { Router, Route, navigate } from "svelte-routing";
   import IconHelp from "carbon-icons-svelte/lib/Help16";
+  import IconHome from "carbon-icons-svelte/lib/Home16";
+  import IconMap from "carbon-icons-svelte/lib/Map16";
+  import IconSettings from "carbon-icons-svelte/lib/Settings16";
 
+  import DashboardRoute from "./routes/Dashboard.svelte";
+  import MapRoute from "./routes/Map.svelte";
   import SettingsRoute from "./routes/Settings.svelte";
   import { Busy } from "./stores/busy";
   import SaveDiscard from "./widget/SaveDiscard.svelte";
@@ -19,18 +25,37 @@
   let helpOpen = false;
 
   const help = () => (helpOpen = true);
+  export let url = "/";
 </script>
 
 {#if $Busy}
   <Loading />
 {/if}
 <Header company="ArduMower" platformName="Modem">
+  <Button
+    on:click={() => { url = "/"; navigate(url, { replace: true }); }}
+    kind="tertiary"
+    icon={IconHome}>
+    Dashboard
+  </Button>
+  <Button
+    on:click={() => { url = "/map"; navigate(url, { replace: true }); }}
+    kind="tertiary"
+    icon={IconMap}>
+    Map
+  </Button>
   <div slot="skip-to-content">
     <SkipToContent />
   </div>
 
   <HeaderUtilities>
     <SaveDiscard />
+    <Button
+      on:click={() => { url = "/settings"; navigate(url, { replace: true }); }}  
+      kind="tertiary"
+      icon={IconSettings}
+      iconDescription="Settings"
+    />
     <Button
       on:click={help}
       kind="tertiary"
@@ -43,7 +68,14 @@
 
 <Content>
   <HelpDialog bind:open={helpOpen} />
-  <SettingsRoute />
+  <Router url="{url}">
+    <div>
+      <Route path="/"  component="{DashboardRoute}" />
+      <Route path="/map" component="{MapRoute}" />
+      <Route path="/settings" component="{SettingsRoute}" /> 
+    </div>
+  </Router>
+  <DashboardRoute />
 </Content>
 
 <style>
---
+++ ui/src/routes/Dashboard.svelte
@@ -0,0 +1,8 @@
+<script>
+import DashboardList from "../pages/daschboard/DashboardList.svelte";
+
+</script>
+
+<DashboardList>
+
+</DashboardList>
